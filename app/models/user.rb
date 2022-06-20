cb7685a2edc446411a72ddfb2f9801290e58b981
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-21 02:21:24 +0900

    rspecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true 
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true   
-         validates :first_name_kana, presence: true    
+         validates :last_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' }  
+         validates :first_name, presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } 
+         validates :last_name_kana, presence: true, format:{ with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+         validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
          validates :birthday, presence: true
 
          has_many :items
