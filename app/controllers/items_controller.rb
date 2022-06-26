4d7eec65927104f61f4df50216fb41fa22e6b335
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-26 18:57:29 +0900

    商品投稿したユーザーと現在ログイン中のユーザー以外が編集ページへいけない編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -41,6 +43,14 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:days_to_ship_id,:item,:description,:price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 
   
