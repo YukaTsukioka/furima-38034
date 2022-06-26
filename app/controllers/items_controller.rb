0ee8c8367c7bb5f1498a8cddb1ac80e51d2da1ab
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-26 19:36:14 +0900

    編集ページに金額が表示される編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update]
-  before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
@@ -21,15 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+   
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
   
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path
     else
