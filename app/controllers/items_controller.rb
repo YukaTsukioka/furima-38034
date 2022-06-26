00a24c91e14e96a2239154f5fcf2f83af92d91b9
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-26 21:31:15 +0900

    削除機能の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update,  :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -37,7 +37,13 @@ class ItemsController < ApplicationController
     end
   end
 
- 
+  def destroy
+    if @item.destroy
+       redirect_to root_path
+    else
+       redirect_to root_path
+    end
+  end
 
 
 
